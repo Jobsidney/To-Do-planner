3aeb7175949463b188d4f0670c5b72c6e38909cc
Jobsidney <jobsidney67@gmail.com>
2022-06-13 17:36:43 +0300

    FIrst toto commit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector('form').addEventListener('submit',(e)=>{
+    console.log(e.target.todo.value)
+    handleTask(e.target.todo.value)
+    e.preventDefault()
+})
+
+function handleTask(work){
+    const p=document.createElement('p');
+    p.innerText=`${work}  `;
+    const item=document.querySelector('#todoDiv')
+    item.appendChild(p)
+    const btn=document.createElement('button');
+    btn.setAttribute('id','#btn')
+    btn.innerHTML='<i class = "fas fa-trash" ></i >'
+    p.appendChild(btn)
+    btn.addEventListener('click',(e)=>{e.target.parentNode.remove()})
+}
+
